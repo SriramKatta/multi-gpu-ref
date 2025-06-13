0d43c2553dd904a3a9596e2a80c2ff883d2c0ec7
Sriram Katta <sriram.katta@fau.de>
2025-06-13 10:01:16 +0200

    Refactor Jacobi solver implementation and add benchmark script for NCCL overlap performance evaluation

--- multigpujacobi/impl/nccl_overlap_graph/jacobi.cu
+++ multigpujacobi/impl/nccl_overlap_graph/jacobi.cu
@@ -8,12 +8,10 @@
 
 #include "utils.h"
 
-// TODO: comment out on my personal machine since it doesn't have cuda aware mpi :(
 #ifndef SKIP_CUDA_AWARENESS_CHECK
 #include <mpi-ext.h>
 #if !defined(MPIX_CUDA_AWARE_SUPPORT) || !MPIX_CUDA_AWARE_SUPPORT
-#error "The used MPI Implementation does not have CUDA-aware support or CUDA-aware \
-support can't be determined. Define SKIP_CUDA_AWARENESS_CHECK to skip this check."
+#error "The used MPI Implementation does not have CUDA-aware support or CUDA-aware support can't be determined. Define SKIP_CUDA_AWARENESS_CHECK to skip this check."
 #endif
 #endif
 
@@ -27,6 +25,7 @@ __global__ void initialize_boundaries(real *__restrict__ const a_new, real *__re
                                       const real pi, const int offset, const int N, const int my_ny);
 void launch_initialize_boundaries(real *__restrict__ const a_new, real *__restrict__ const a,
                                   const real pi, const int offset, const int N, const int my_ny);
+
 template <int BLOCK_DIM_X, int BLOCK_DIM_Y>
 __global__ void jacobi_kernel(real *__restrict__ const a_new, const real *__restrict__ const a,
                               const int iy_start, const int iy_end, const int N);
@@ -37,34 +36,22 @@ void Halo_exchange(real *a, real *a_new, int N, const int top, int iy_end, const
 int main(int argc, char *argv[])
 {
   MPI_CALL(MPI_Init(&argc, &argv));
-  int rank = 0;
-  int nranks = 1;
-  int num_devices = 0;
-  // assumption is that we are only on single node but easily extendable using MPI_Comm_split_type(.... MPI_COMM_TYPE_SHARED...
-  {
-    MPI_CALL(MPI_Comm_rank(MPI_COMM_WORLD, &rank));
-    MPI_CALL(MPI_Comm_size(MPI_COMM_WORLD, &nranks));
-  }
+  int rank = 0, nranks = 1;
+  CUDA_CALL(cudaGetDeviceCount(&nranks));
+  MPI_CALL(MPI_Comm_rank(MPI_COMM_WORLD, &rank));
+  MPI_CALL(MPI_Comm_size(MPI_COMM_WORLD, &nranks));
 
-  // important part of cuda aware mpi
-  {
-    CUDA_CALL(cudaGetDeviceCount(&num_devices));
-    CUDA_CALL(cudaSetDevice(rank % num_devices));
-    CUDA_CALL(cudaFree(0));
-  }
+  CUDA_CALL(cudaSetDevice(rank % nranks));
+  CUDA_CALL(cudaFree(0));
 
-  // pass the communication hadling to NCCL
   ncclComm_t ncclcomm;
   ncclUniqueId id;
   if (rank == 0)
     NCCL_CALL(ncclGetUniqueId(&id));
   MPI_CALL(MPI_Bcast(&id, sizeof(id), MPI_BYTE, 0, MPI_COMM_WORLD));
   NCCL_CALL(ncclCommInitRank(&ncclcomm, nranks, id, rank));
-
-  // just to besafe
   MPI_CALL(MPI_Barrier(MPI_COMM_WORLD));
 
-  // just to simplyfy the program
   int N = 1024;
   if (argc > 1)
   {
@@ -78,124 +65,127 @@ int main(int argc, char *argv[])
     }
   }
 
-  // computing local chunk size
   int chunk_size = rowsinrank(rank, nranks, N);
+  real *a, *a_new;
+  CUDA_CALL(cudaMalloc(&a, N * (chunk_size + 2) * sizeof(real)));
+  CUDA_CALL(cudaMemset(a, 0, N * (chunk_size + 2) * sizeof(real)));
+  CUDA_CALL(cudaMalloc(&a_new, N * (chunk_size + 2) * sizeof(real)));
+  CUDA_CALL(cudaMemset(a_new, 0, N * (chunk_size + 2) * sizeof(real)));
 
-  // setting up data for each GPU
-  real *a;
-  real *a_new;
-  // clang-format off
-  CUDA_CALL(cudaMalloc(&a     , N * (chunk_size + 2) * sizeof(real)));
-  CUDA_CALL(cudaMemset(a   , 0, N * (chunk_size + 2) * sizeof(real)));
-  CUDA_CALL(cudaMalloc(&a_new , N * (chunk_size + 2) * sizeof(real)));
-  CUDA_CALL(cudaMemset(a_new,0, N * (chunk_size + 2) * sizeof(real)));
-  // clang-format on
-
-  // Calculate local domain boundaries
   int iy_start_global = startrow(rank, nranks, N);
-
   int iy_start = 1;
   int iy_end = iy_start + chunk_size;
 
-  int highpriority = 0, lowpriority = 0;
-  cudaStream_t inner_stream;
-  cudaStream_t edge_stream;
-  CUDA_CALL(cudaDeviceGetStreamPriorityRange(&lowpriority, &highpriority));
-  CUDA_CALL(cudaStreamCreateWithPriority(&inner_stream, cudaStreamDefault, lowpriority));
-  CUDA_CALL(cudaStreamCreateWithPriority(&edge_stream, cudaStreamDefault, highpriority));
-  cudaEvent_t inner_done;
-  cudaEvent_t edge_done;
+  // Streams with different priorities
+  int lowp, highp;
+  cudaStream_t inner_stream, edge_stream;
+  CUDA_CALL(cudaDeviceGetStreamPriorityRange(&lowp, &highp));
+  CUDA_CALL(cudaStreamCreateWithPriority(&inner_stream, cudaStreamDefault, lowp));
+  CUDA_CALL(cudaStreamCreateWithPriority(&edge_stream, cudaStreamDefault, highp));
+  cudaEvent_t inner_done, edge_done;
   CUDA_CALL(cudaEventCreateWithFlags(&inner_done, cudaEventDisableTiming));
   CUDA_CALL(cudaEventCreateWithFlags(&edge_done, cudaEventDisableTiming));
 
   const int top_pe = (rank + 1) % nranks;
   const int bot_pe = (rank + nranks - 1) % nranks;
 
+  // Warmup NCCL+halo exchanges
   nvtxRangePushA("NCCL_Warmup");
-  for (size_t i = 0; i < number_of_warmups; i++)
+
+  for (int i = 0; i < number_of_warmups; ++i)
   {
     Halo_exchange(a_new, a, N, top_pe, iy_end, bot_pe, iy_start, ncclcomm, edge_stream);
     std::swap(a, a_new);
   }
-  nvtxRangePop();
   MPI_CALL(MPI_Barrier(MPI_COMM_WORLD));
   CUDA_CALL(cudaDeviceSynchronize());
+  nvtxRangePop();
 
-  cudaGraphExec_t graph_exec[2];
-
-  nvtxRangePushA("Graph_creation");
+  cudaGraph_t graphs[2];
+  nvtxRangePushA("Graph_create");
+  for (int g = 0; g < 2; ++g)
   {
-    cudaGraph_t graphs[2];
-    for (size_t i = 0; i < 2; i++)
-    {
-      CUDA_CALL(cudaStreamBeginCapture(inner_stream, cudaStreamCaptureModeGlobal));
-      launch_jacobi_kernel(a_new, a, iy_start + 1, iy_end - 1, N, inner_stream);
-      CUDA_CALL(cudaEventRecord(inner_done, inner_stream));
-      launch_jacobi_kernel(a_new, a, iy_start, iy_start + 1, N, edge_stream);
-      launch_jacobi_kernel(a_new, a, iy_end - 1, iy_end, N, edge_stream);
-      NCCL_CALL(ncclGroupStart());
-      // clang-format off
-      NCCL_CALL(ncclRecv(a_new                   , N, NCCL_REAL_TYPE, top_pe , ncclcomm, edge_stream));
-      NCCL_CALL(ncclSend(a_new + (iy_end - 1) * N, N, NCCL_REAL_TYPE, bot_pe , ncclcomm, edge_stream));
-      NCCL_CALL(ncclRecv(a_new + (iy_end * N)    , N, NCCL_REAL_TYPE, bot_pe , ncclcomm, edge_stream));
-      NCCL_CALL(ncclSend(a_new + (iy_start * N)  , N, NCCL_REAL_TYPE, top_pe , ncclcomm, edge_stream));
-      // clang-format on
-      NCCL_CALL(ncclGroupEnd());
-      CUDA_CALL(cudaEventRecord(edge_done, edge_stream));
-      CUDA_CALL(cudaStreamWaitEvent(inner_stream, edge_done, 0));
-      CUDA_CALL(cudaStreamEndCapture(inner_stream, graphs + i));
-      std::swap(a, a_new);
-    }
-    {
-      CUDA_CALL(cudaGraphInstantiateWithFlags(&graph_exec[0], graphs[0], cudaGraphInstantiateFlagUseNodePriority));
-      CUDA_CALL(cudaGraphInstantiateWithFlags(&graph_exec[1], graphs[1], cudaGraphInstantiateFlagUseNodePriority));
-    }
-    // cudaGraph_t objects are no longer required after instantiation
-    CUDA_CALL(cudaGraphDestroy(graphs[0]));
-    CUDA_CALL(cudaGraphDestroy(graphs[1]));
+    // Begin global capture on both streams
+    CUDA_CALL(cudaStreamBeginCapture(inner_stream, cudaStreamCaptureModeGlobal));
+    CUDA_CALL(cudaStreamBeginCapture(edge_stream, cudaStreamCaptureModeGlobal));
+
+    // Launch edge-row Jacobi on edge_stream
+    launch_jacobi_kernel(a_new, a, iy_start, iy_start + 1, N, edge_stream);
+    launch_jacobi_kernel(a_new, a, iy_end - 1, iy_end, N, edge_stream);
+
+    // NCCL halo exchange on edge_stream
+    NCCL_CALL(ncclGroupStart());
+    NCCL_CALL(ncclRecv(a_new, N, NCCL_REAL_TYPE, top_pe, ncclcomm, edge_stream));
+    NCCL_CALL(ncclSend(a_new + (iy_end - 1) * N, N, NCCL_REAL_TYPE, bot_pe, ncclcomm, edge_stream));
+    NCCL_CALL(ncclRecv(a_new + iy_end * N, N, NCCL_REAL_TYPE, bot_pe, ncclcomm, edge_stream));
+    NCCL_CALL(ncclSend(a_new + iy_start * N, N, NCCL_REAL_TYPE, top_pe, ncclcomm, edge_stream));
+    NCCL_CALL(ncclGroupEnd());
+
+    // Inner Jacobi on inner_stream
+    launch_jacobi_kernel(a_new, a, iy_start + 1, iy_end - 1, N, inner_stream);
+
+    // Capture end: first end on edge_stream to get full graph, then on inner_stream
+    CUDA_CALL(cudaStreamEndCapture(edge_stream, &graphs[g]));
+    CUDA_CALL(cudaStreamEndCapture(inner_stream, nullptr));
+
+    std::swap(a, a_new);
   }
   nvtxRangePop();
 
-  nvtxRangePushA("Graph_warmup");
+  // Instantiate graphs
+
+  cudaGraphExec_t graph_exec[2];
+  nvtxRangePushA("Graph_init");
+  for (int g = 0; g < 2; ++g)
+  {
+    CUDA_CALL(cudaGraphInstantiateWithFlags(&graph_exec[g], graphs[g], cudaGraphInstantiateFlagUseNodePriority));
+    CUDA_CALL(cudaGraphDestroy(graphs[g]));
+  }
+  nvtxRangePop();
+
+  // Warmup graph launches
+  nvtxRangePushA("Graph_Warmup");
   for (int i = 0; i < 10; ++i)
   {
-    CUDA_CALL(cudaGraphUpload(graph_exec[0], inner_stream));
-    CUDA_CALL(cudaGraphUpload(graph_exec[1], inner_stream));
+    CUDA_CALL(cudaGraphLaunch(graph_exec[0], inner_stream));
+    CUDA_CALL(cudaGraphLaunch(graph_exec[1], inner_stream));
     CUDA_CALL(cudaStreamSynchronize(inner_stream));
   }
   nvtxRangePop();
 
+  // Initialize boundaries
   CUDA_CALL(cudaMemset(a, 0, N * (chunk_size + 2) * sizeof(real)));
   CUDA_CALL(cudaMemset(a_new, 0, N * (chunk_size + 2) * sizeof(real)));
-  launch_initialize_boundaries(a, a_new, M_PI, iy_start_global - 1, N, (chunk_size + 2));
+  launch_initialize_boundaries(a, a_new, M_PI, iy_start_global - 1, N, chunk_size + 2);
   CUDA_CALL(cudaDeviceSynchronize());
 
+  // Solve
   double start = MPI_Wtime();
-  nvtxRangePushA("Jacobi_solve");
-  for (size_t it = 0; it < maxIt; ++it)
+  nvtxRangePushA("Jacobistep");
+  for (int it = 0; it < maxIt; ++it)
   {
     CUDA_CALL(cudaGraphLaunch(graph_exec[it % 2], inner_stream));
   }
   CUDA_CALL(cudaDeviceSynchronize());
   nvtxRangePop();
-
   double dur = (MPI_Wtime() - start) / maxIt;
-  double maxdur = 0.0;
+  double maxdur = 0;
   MPI_CALL(MPI_Reduce(&dur, &maxdur, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD));
 
   if (rank == 0)
   {
-    printf("NP %3d | LUPs %12d | perf %7.3f MLUPS/s\n", nranks, (N * N), static_cast<double>(N * N) / maxdur / 1e6);
+    printf("NP %3d | LUPs %12d | perf %7.3f MLUPS/s\n", nranks, (N * N), N * N / maxdur / 1e6);
   }
 
-  // freeing everything
+  // Cleanup
   CUDA_CALL(cudaFree(a));
   CUDA_CALL(cudaFree(a_new));
+  CUDA_CALL(cudaGraphExecDestroy(graph_exec[1]));
+  CUDA_CALL(cudaGraphExecDestroy(graph_exec[0]));
+  CUDA_CALL(cudaEventDestroy(inner_done));
+  CUDA_CALL(cudaEventDestroy(edge_done));
   CUDA_CALL(cudaStreamDestroy(inner_stream));
   CUDA_CALL(cudaStreamDestroy(edge_stream));
-  CUDA_CALL(cudaEventDestroy(edge_done));
-  CUDA_CALL(cudaEventDestroy(inner_done));
-
   NCCL_CALL(ncclCommDestroy(ncclcomm));
   MPI_Finalize();
   return 0;
---
+++ multigpujacobi/nccl_overlap_graph_bench.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -l
+#
+#SBATCH --output=./SLURM_OUT_FILES/%j_%x.out
+#SBATCH --nodes=1
+#SBATCH --time=0:59:00
+#SBATCH --exclusive
+#SBATCH --export=NONE
+
+unset SLURM_EXPORT_ENV
+
+gpgpucount=$(nvidia-smi --query-gpu=gpu_name --format=csv | grep -i "nvidia" | wc -l)
+
+module purge
+module load cuda
+module load likwid
+module load openmpi/4.1.6-nvhpc23.7-cuda12
+
+export NV_COMM_LIBS=$NVHPC_ROOT/Linux_x86_64/23.7/comm_libs/
+#load NCCL library
+export NCCL_HOME=$NV_COMM_LIBS/nccl
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$NCCL_HOME/lib
+
+[ ! -d simdata ] && mkdir simdata
+resfile=./simdata/${SLURM_JOB_ID}_caware
+
+perfexemain="./executable_perf/jacobi_NCCL_overlap_graph"
+profexemain="./executable_prof/jacobi_NCCL_overlap_graph"
+
+perfexe="./executable_perf/jacobi_NCCL_overlap_graph_${SLURM_JOB_ID}"
+profexe="./executable_prof/jacobi_NCCL_overlap_graph_${SLURM_JOB_ID}"
+
+cp $perfexemain $perfexe
+cp $profexemain $profexe
+
+for np in $(seq 1 $gpgpucount); do
+    echo "$np of $gpgpucount"
+    likwid-mpirun -np $np -nperdomain M:1 $perfexe 40960 | tee -a $resfile
+done
+
+nsys profile --trace=mpi,cuda,nvtx --cuda-graph-trace=node --force-overwrite true \
+    -o ./simdata/${SLURM_JOB_ID}_jacobi_NCCL_overlap_graph \
+    likwid-mpirun -np $gpgpucount -nperdomain M:1 \
+    $profexe 4096
+
+rm $profexe
+rm $perfexe
